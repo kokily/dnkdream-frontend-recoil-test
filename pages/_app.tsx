c7e5b4a8f63235466d56c4ab75bb2770bff608b4
kokily <kokily@naver.com>
2021-09-30 07:47:41 +0900

    install packages, Global state Recoil SSR config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
 import { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+
+// Recoil SSR config
+const allAtoms = {};
+
+const initRecoilState =
+  (initialState) =>
+  ({ set }) =>
+    Object.keys(initialState).map((key) => {
+      const value = initialState[key];
+      const atom = allAtoms[key];
+
+      set(atom, value);
+    });
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <RecoilRoot initializeState={initRecoilState({})}>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </>
   );
 }
